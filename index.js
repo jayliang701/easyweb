603f9b0c90f029d909034ccb80f18dbf199a454f
156070304@qq.com <156070304@qq.com>
2015-09-02 13:41:07 +0800

    update

--- index.js
+++ index.js
@@ -6,3 +6,29 @@ exports.model = {
 	Redi: require("./model/Redis"),
 	Session: require("./model/Session")
 }
+
+exports.net = {
+	EdgeServiceProxy: require("./net/EdgeServiceProxy"),
+	HTTPRequest: require("./net/HTTPRequest"),
+	HTTPService: require("./net/HTTPService"),
+	SocketClient: require("./net/SocketClient"),
+	SocketConnector: require("./net/SocketConnector"),
+	SocketPacket: require("./net/SocketPacket"),
+	SocketRequest: require("./net/SocketRequest"),
+	SocketServer: require("./net/SocketServer"),
+	TCPRequest: require("./net/TCPRequest")
+}
+
+exports.schedule = {
+	ScheduleManager: require("./schedule/ScheduleManager")
+}
+
+exports.utils = {
+	Captcha: require("./utils/Captcha"),
+	Logger: require("./utils/Logger"),
+	ShellUtil: require("./utils/ShellUtil"),
+	SMSUtil: require("./utils/SMSUtil"),
+	SwigFilter: require("./utils/SwigFilter"),
+	TemplateLib: require("./utils/TemplateLib"),
+	Utils: require("./utils/Utils")
+}
