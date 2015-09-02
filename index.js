681cd872c510d57fb11ec2430cfc753d2041d7c5
156070304@qq.com <156070304@qq.com>
2015-09-02 13:26:02 +0800

    update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 exports.model = {
-	"MemoryCache": require("model/MemoryCache")
-	"Model": require("model/Model")
-	"MongoDB": require("model/MongoDB")
-	"Redis": require("model/Redis")
-	"Session": require("model/Session")
+	MemoryCache: require("./model/MemoryCache"),
+	Model: require("./model/Model"),
+	MongoDB: require("./model/MongoDB"),
+	Redi: require("./model/Redis"),
+	Session: require("./model/Session")
 }
